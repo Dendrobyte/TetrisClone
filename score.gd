5af92c496fc3164e17caf7d6aa6c21cbe099a43e
Dendrobyte <markbacon78@gmail.com>
2024-01-11 15:33:30 -0800

    point multiplier added

--- score.gd
+++ score.gd
@@ -4,7 +4,5 @@ var score = 0
 
 
 func _on_main_line_cleared(lines_cleared):
-	score += 10 * lines_cleared
-	if lines_cleared > 1:
-		score += 20 * (lines_cleared / 8)  # Could be better, idk
+	score += (10 * lines_cleared) + (5 * (lines_cleared - 1))
 	text = "Score: %s" % score
